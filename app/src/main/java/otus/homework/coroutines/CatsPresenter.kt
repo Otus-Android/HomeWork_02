bafae49299f53547cb71c6d82fa1b76785fc0b89
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-04-13 15:44:17 +0300

    Migrate from callbacks to coroutines

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,35 +1,52 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import java.net.SocketTimeoutException
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class CatsPresenter(
-    private val catsService: CatsService
+    private val catsService : CatsService,
 ) {
+
+    private val presenterScope = CoroutineScope(Dispatchers.Main + CoroutineName("CatsCoroutine"))
 
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
+        presenterScope.launch {
+            val fact = async(Dispatchers.IO) {
+                try {
+                    catsService.getCatFact()
+                } catch (e : SocketTimeoutException) {
+                    withContext(Dispatchers.Main) {
+                        _catsView?.showToast("Не удалось получить ответ от сервером")
+                    }
+                    throw CancellationException()
+                } catch (e : Exception) {
+                    CrashMonitor.trackWarning()
+                    withContext(Dispatchers.Main) {
+                        _catsView?.showToast(e.message ?: "Uknown exception")
+                    }
+                    throw CancellationException()
                 }
             }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+            _catsView?.populate(fact.await())
+        }
     }
 
-    fun attachView(catsView: ICatsView) {
+    fun attachView(catsView : ICatsView) {
         _catsView = catsView
     }
 
     fun detachView() {
         _catsView = null
+        presenterScope.cancel()
     }
 }
