71a2b8f2844f2ba61feac20c5332aca90b2b311c
Ponyu-dev <mopoypa@yandex.ru>
2024-04-20 07:14:48 +0300

    Реализовал Пункт 1. Перейти с коллбеков на suspend функции и корутины.

--- app/src/main/java/otus/homework/coroutines/CrashMonitor.kt
+++ app/src/main/java/otus/homework/coroutines/CrashMonitor.kt
@@ -1,10 +1,17 @@
 package otus.homework.coroutines
 
+import android.util.Log
+
 object CrashMonitor {
+
+    private const val TAG = "CrashMonitor"
 
     /**
      * Pretend this is Crashlytics/AppCenter
      */
-    fun trackWarning() {
+    fun trackWarning(
+        message: String
+    ) {
+        Log.w(TAG, message)
     }
 }
--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -2,10 +2,12 @@ package otus.homework.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import java.net.SocketTimeoutException
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var catsPresenter: CatsPresenter
+    private lateinit var catsPresenter: CatsPresenter
 
     private val diContainer = DiContainer()
 
@@ -15,16 +17,25 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsPresenter = CatsPresenter(diContainer.service)
+        catsPresenter = CatsPresenter(diContainer.service) {
+            onShowErrorMessage(if (it is SocketTimeoutException) getString(R.string.timeout_exception_message) else it.localizedMessage.orEmpty())
+        }
         view.presenter = catsPresenter
         catsPresenter.attachView(view)
         catsPresenter.onInitComplete()
     }
 
     override fun onStop() {
+        catsPresenter.cancelJob()
+
         if (isFinishing) {
             catsPresenter.detachView()
         }
+
         super.onStop()
+    }
+
+    private fun onShowErrorMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
