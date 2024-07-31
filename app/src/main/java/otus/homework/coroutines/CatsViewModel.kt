02ecea3ae255ad33afe676b612d36ba2d44d6f93
Ponyu-dev <mopoypa@yandex.ru>
2024-07-31 16:14:01 +0300

    Убрал у CatsViewModel наследование от ICatsPresenter. И стал там использовать livedata.

--- app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
+++ app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
@@ -1,34 +1,34 @@
 package otus.homework.coroutines
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.net.SocketTimeoutException
 
 class CatsViewModel(
-    private val apiService: ApiService,
-    private val onShowErrorMessage: (Throwable) -> Unit
-) : ViewModel(), ICatsPresenter {
+    private val apiService: ApiService
+) : ViewModel(), ICatsRefresh {
 
-    override var _catsView: ICatsView? = null
-    override var job: Job? = null
+    private val _catsLiveData = MutableLiveData<Result>()
+    val catsLiveData: LiveData<Result> = _catsLiveData
 
     override fun onInitComplete() {
         viewModelScope.launch {
             try {
-                val factResult = async { apiService.serviceCatFact.getCatFact().fact }
+                val factResult =
+                    async { apiService.serviceCatFact.getCatFact().fact }
                 val imageResult =
                     async { apiService.serviceCatImage.getCatImage().firstOrNull()?.url.orEmpty() }
+
                 val catData =
                     CatData(
                         fact = factResult.await(),
                         imageUrl = imageResult.await()
                     )
-
-                _catsView?.populate(Result.Success(catData))
+                _catsLiveData.value = Success(catData)
             }
             catch (e : Exception){
                 showError(e)
@@ -37,7 +37,7 @@ class CatsViewModel(
     }
 
     private fun showError(throwable: Throwable) {
-        onShowErrorMessage(throwable)
+        _catsLiveData.value = Error(throwable.localizedMessage.orEmpty())
 
         if (throwable is SocketTimeoutException) return
 
